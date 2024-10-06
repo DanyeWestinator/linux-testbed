99aa5b3560709d34618205ed320385e46ebdbe44
DanyeWestinator <dcarsten@uci.edu>
2024-10-06 14:29:34 -0700

    Moved screensaver logic into repo

---
+++ screensaver.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+#Boots a random screensaver
+num_savers=4
+rand=$((1 + $RANDOM % $num_savers))
+#rand=4
+#first choice is asciiquarium
+aquarium () {
+    ~/linux-testbed/asciiquarium
+}
+#2 is pipes
+pipes () {
+     ~/linux-testbed/pipes.sh
+}
+matrix () {
+     cmatrix -b -s
+}
+bonsai () {
+    clear
+    cd ~/linux-testbed/cbonsai
+    #message=$(fortune -s)
+ cbonsai -w 1.00 --live  -m "Grower, not a show-er" -t 0.3 --leaf="&,#,%,(,),!" -M 8 -i
+}
+
+if [ $rand == 1 ]; then
+    aquarium
+fi
+
+if [ $rand == 2 ]; then
+    pipes
+fi
+
+if [ $rand == 3 ]; then
+    matrix
+fi
+
+if [ $rand == 4 ]; then
+    bonsai
+fi
+
